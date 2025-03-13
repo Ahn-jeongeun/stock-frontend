162ed1e719b08bb325e5abf6d98c5a2afefdf5d1
Yongyeon Kim <seyeon.dev@gmail.com>
2025-03-13 18:15:04 +0900

    feat: polygon 웹소켓 설정

--- js/axios.js
+++ js/axios.js
@@ -2,7 +2,7 @@ import { POLY_API_KEY } from './confjg.js';
 
 const polyAxios = axios.create({
   baseURL: 'https://api.polygon.io',
-  headers: { Authorization: POLY_API_KEY },
+  headers: { Authorization: 'Bearer ' + POLY_API_KEY },
 });
 
 export { polyAxios };
