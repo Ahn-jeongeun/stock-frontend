aac46160c63e78a387cab61aca9d9d31d712583e
Yongyeon Kim <seyeon.dev@gmail.com>
2025-03-14 14:01:41 +0900

    style: 모달 여닫기 구현

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const signInBtn = document.querySelector('.signInBtn');
+const signUpBtn = document.querySelector('.signUpBtn');
+const modalDiv = document.querySelector('#modal');
+const modalOverlay = document.querySelector('#overlay');
+
+signInBtn.addEventListener('click', (_) => {
+  // 모달 열기
+  modalDiv.style.display = 'block';
+});
+
+signUpBtn.addEventListener('click', (_) => {
+  // 모달 열기
+  modalDiv.style.display = 'block';
+});
+
+// 모달 바깥 쪽 누르면 모달 닫기
+modalOverlay.addEventListener('click', (e) => {
+  modalDiv.style.display = 'none';
+});
